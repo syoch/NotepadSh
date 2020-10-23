180a7f1e91dc272d6a84d52388c0fc8b449d0fd9
syoch <hacksyoch@gmail.com>
2020-10-23 21:24:21 +0900

    要らないかっこを削除

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ class MyApp : public wxApp
 public:
     virtual bool OnInit()
     {
-        {
-            MyFrame *frame = new MyFrame();
-            frame->Show(true);
-            return true;
-        }
+        MyFrame *frame = new MyFrame();
+        frame->Show(true);
+        return true;
     }
 };
 wxIMPLEMENT_APP(MyApp);
