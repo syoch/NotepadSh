c283e0da86486bd471cd758769de7d35fd854316
syoch <hacksyoch@gmail.com>
2020-10-31 06:09:41 +0900

    バッファー関数一式の定義

--- inc/command.h
+++ inc/command.h
@@ -1,5 +1,17 @@
+#include "pch.h"
+
 #pragma once
 #ifndef COMMAND_H
 #define COMMAND_H
+
+namespace command{
+    namespace buffer
+    {
+        extern wxString buffer;
+        bool add(wxString);
+        bool remove(wxString);
+    } // namespace buffer
+    
+}
 
 #endif // !defined(COMMAND_H)
--- src/command.cpp
+++ src/command.cpp
@@ -1,2 +1,13 @@
 #include "pch.h"
 #include "command.h"
+
+// buffer: wxString command::buffer::buffer;
+
+bool command::buffer::add(wxString str){
+    std::cout<<"add"<< str;
+    return true;
+}
+bool command::buffer::remove(wxString str){
+    std::cout<<"rem"<<str;
+    return true;
+}
