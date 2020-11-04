f4641a0e3e526ac1521f62218efff39623d31a47
syoch <hacksyoch@gmail.com>
2020-11-04 19:31:20 +0900

    不要な出力の削除

--- src/command.cpp
+++ src/command.cpp
@@ -12,12 +12,10 @@ void command::update(){
 
     long insertPos=target->texteditor->GetInsertionPoint();
     if(text.substr(text.length()-3,(size_t)insertPos)==">>>" && isInCommand==false){
-        std::cout<<"start!!!"<<std::endl;
         start=insertPos;
         isInCommand=true;
     }
     if(text.substr(text.length()-3,(size_t)insertPos)=="<<<" && isInCommand==true){
-        std::cout<<"end!!!"<<std::endl;
         wxString command=text.SubString(start,insertPos-4);
         isInCommand=false;
     }
