df89867c3c50628982ede5725e48b068eb6599ed
syoch <hacksyoch@gmail.com>
2020-11-04 15:40:05 +0900

    コマンド入力モードのフラグ（静的変数）を実装

--- src/command.cpp
+++ src/command.cpp
@@ -5,15 +5,18 @@ command::command(){
 }
 
 void command::update(){
-    
+    static bool isInCommand;
     wxString text=target->texteditor->GetValue();
     if(text.length() < 3)return;
     
     long insertPos=target->texteditor->GetInsertionPoint();
-    if(text.substr(text.length()-3,(size_t)insertPos)==">>>"){
+    if(text.substr(text.length()-3,(size_t)insertPos)==">>>" && isInCommand==false){
         std::cout<<"start!!!"<<std::endl;
+        isInCommand=true;
     }
-    if(text.substr(text.length()-3,(size_t)insertPos)=="<<<"){
-        std::cout<<"stop!!!"<<std::endl;
+    if(text.substr(text.length()-3,(size_t)insertPos)=="<<<" && isInCommand==true){
+        std::cout<<"end!!!"<<std::endl;
+        isInCommand=false;
     }
+
 }
