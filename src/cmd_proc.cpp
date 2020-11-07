abe1eed126e57258f9a48e5afe92809b3258a31f
syoch <hacksyoch@gmail.com>
2020-11-07 18:58:43 +0900

    トークライザの書き直し#1$数値の読み込みの実装

--- src/cmd_proc.cpp
+++ src/cmd_proc.cpp
@@ -11,30 +11,20 @@ commandProcessor::target* commandProcessor::target::tokenize()
 {
     // split
     wxString buffer;
-    char ch;
+    wxUniChar ch;
     size_t i;
     for (i = 0; i < src.length(); i++)
     {
         ch = src[i];
-        if (ch == ' ' || ch == ',' || ch == '(' || ch == ')')
+        if(wxIsdigit(ch))
         {
-            if (!buffer.empty())
-                tokens.push_back(std::make_pair(UNKNOWN,buffer));
-            if (ch != ' ')
-                tokens.push_back(std::make_pair(UNKNOWN,ch));
-            buffer = "";
-            continue;
-        }
-        buffer += ch;
-        if (ch == '\'')
-        {
-            i++; // skip first '
-            size_t before = i;
-            while (src[i] != '\'')
-                i++;
-            tokens.push_back(std::make_pair(UNKNOWN,buffer + src.substr(before, i)));
-            buffer = "";
-            //i+=1; // skip last '
+            size_t start=i;
+            while(wxIsdigit(src[i]))i++;
+            i--;
+
+            std::cout<<"n"<<src.SubString(start,i)<<std::endl;
+        }else{
+            std::cout<<" "<<wxString(ch)<<std::endl;
         }
     }
     if (!buffer.empty())
