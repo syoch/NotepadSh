176967bfed1e5a93db9cf30022f5fabf66e202c8
syoch <hacksyoch@gmail.com>
2021-01-12 16:04:06 +0900

    デバッグに使用するコメントの追加

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -7,5 +7,9 @@ bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame();
     frame->Show(true);
+    //frame->texteditor->WriteText(">>>");
+    //frame->texteditor->WriteText("for i in (range(1,10,sin(pi/2))) '{i}, {sin(i)}\\n'");
+    //frame->texteditor->WriteText("<<<");
+    //frame->Close();
     return true;
 }
